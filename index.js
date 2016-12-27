243c79cfb58b44c7a12dbbb4ba080846a5c40e87
Phan Đức Việt <viet@techmaster.vn>
2016-12-27 16:53:52 +0700

    cap nhat lai ten bien

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var path = require('path');
 
 module.exports = function (options) {
     let app = this;
+    app.plugin.zmq = {}
     let logger = app.logger;
     let prefix = options.sub_prefix;
     global.__base = app.rootFolder;
@@ -22,10 +23,10 @@ module.exports = function (options) {
         logger.info(`Connected to zmq publisher at ${options.pub_address}.`);
         return sub;
     }).then(function (mq) {
-        app.sub = mq;
+        app.plugin.zmq.sub = mq;
         return loadMessageRoutes();
     }).then(function () {
-        app.sub.on('message', function (data) {
+        app.plugin.zmq.sub.on('message', function (data) {
             data = data.toString("utf8");
             if (!data.startsWith(prefix))
                 return;
@@ -36,9 +37,9 @@ module.exports = function (options) {
                 let type = message.payload.type;
                 let route = "/" + from + "/" + type;
 
-                let handler = app.messageRoute[route];
+                let handler = app.plugin.zmq.messageRoute[route];
                 if (!handler)
-                    throw new Error("Route không tồn tại: "+ route);
+                    throw new Error("Route không tồn tại: " + route);
                 handler(message);
             } catch (err) {
                 return logger.error(err);
@@ -48,7 +49,7 @@ module.exports = function (options) {
     })
 
     function loadMessageRoutes() {
-        app.messageRoute = {}
+        app.plugin.zmq.messageRoute = {}
         let files = glob.sync(`${__base}/controller/socket/*/route.js`);
         return Promise.map(files, function (filePath) {
             let controllerName = path.dirname(filePath).split("/").pop();
@@ -64,7 +65,7 @@ module.exports = function (options) {
     }
 
     function handleMessageRoute(route, handler) {
-        app.messageRoute[route] = handler;
+        app.plugin.zmq.messageRoute[route] = handler;
     }
 
 }
